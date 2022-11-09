2d7722b2f6c9f4848b89b81ff926687f892e7093
Eleanor Ruth <ellie.ruth@me.com>
2022-11-09 14:51:18 -0700

    Room class

---
+++ src/classes/Room.js
@@ -0,0 +1,18 @@
+class Room {
+  constructor(roomInfo) {
+    this.roomNumber = roomInfo.roomNumber
+    this.roomType = roomInfo.roomType
+    this.bidetStatus = roomInfo.bidet
+    this.bedSize = roomInfo.bedSize
+    this.numBeds = roomInfo.numBeds
+    this.costPerNight = roomInfo.costPerNight
+    // this.allBookingsInfo = allBookingsInfo
+    this.bookingsInfo = []
+  }
+  getBookingInfo() {
+    const currentRoomBookings = this.allBookingsInfo.filter((booking)=>{
+      return booking.roomNumber === this.roomNumber
+    })
+    this.bookingsInfo = currentRoomBookings
+  }
+}
