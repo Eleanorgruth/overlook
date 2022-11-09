aaebd2643cb1e71e546503fcc37a3e571f4fdda8
Eleanor Ruth <ellie.ruth@me.com>
2022-11-09 13:38:31 -0700

    Add file

---
+++ src/domUpdates.js
@@ -0,0 +1,28 @@
+// import './css/styles.css'
+
+// const myBooking = document.querySelector('#myBookings')
+// const bookARoom = document.querySelector('#bookARoom')
+// const bookingForm = document.querySelector('#bookingForm')
+// const bookingOptions = document.querySelector('#bookigInfo')
+
+// bookARoom.addEventListener('click', displayBookingOptions)
+
+
+// function displayBookingOptions() {
+//   console.log("hello")
+//   show([bookingForm, bookingOptions])
+// }
+
+// function hide(elementList) {
+//   elementList.forEach((currentElement) => {
+//       currentElement.classList.add('hidden')
+//   })
+// }
+
+// function show(elementList) {
+//   elementList.forEach((currentElement) => {
+//       currentElement.classList.remove('hidden')
+//   })
+// }
+
+// export default domUpdates
