70277c2039447a0f010257bada41ce9d544e3b27
Eleanor Ruth <ellie.ruth@me.com>
2022-11-09 17:59:48 -0700

    Added Temporary randomizeUser function

--- src/scripts.js
+++ src/scripts.js
@@ -8,50 +8,16 @@ import './css/styles.css';
 //(also need to link to it in the index.html)
 import './images/turing-logo.png'
 import './images/overlook-background-image.png'
+import Customer from './classes/Customer';
+import { displayWelcomeMessage } from './domUpdates';
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
 
-const myBookingView = document.querySelector('#myBookings')
-const bookARoomView = document.querySelector('#bookARoom')
-const bookingForm = document.querySelector('#bookingForm')
-const bookingOptions = document.querySelector('#bookingInfo')
-const myBookings = document.querySelector('#bookings')
-
-bookARoomView.addEventListener('click', displayBookingOptions)
-myBookingView.addEventListener('click', displayMyBookings)
-
-
-function displayBookingOptions() {
-  show([bookingForm, bookingOptions])
-  hide([myBookings])
-  selected(bookARoomView)
-  unselected(myBookingView)
+//Other Functions
+function randomizeUser(data) {
+  randomUser = data[Math.floor(Math.random() * data.length)]
+  customer = new Customer(randomUser)
+  displayWelcomeMessage(customer.name)
+  return user
 }
-
-function displayMyBookings() {
-  selected(myBookingView)
-  unselected(bookARoomView)
-  hide([bookingForm, bookingOptions])
-  show([myBookings])
-}
-
-
-//DOM HELPER FUNCTIONS
-function hide(elementList) {
-  elementList.forEach((currentElement) => {
-      currentElement.classList.add('hidden')
-  })
-}
-
-function show(elementList) {
-  elementList.forEach((currentElement) => {
-      currentElement.classList.remove('hidden')
-  })
-}
-function selected(currentElement) {
-      currentElement.classList.add('selected')
-}
-function unselected(currentElement) {
-      currentElement.classList.remove('selected')
-  }
