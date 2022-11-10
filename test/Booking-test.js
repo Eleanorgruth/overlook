4bb9d767a493744b8177938d0df89853054ff322
Eleanor Ruth <ellie.ruth@me.com>
2022-11-10 15:59:28 -0700

    Add another data point to all tests

--- test/Booking-test.js
+++ test/Booking-test.js
@@ -1,23 +1,20 @@
 import chai from 'chai';
 import Room from '../src/classes/Room';
 import Booking from '../src/classes/Booking';
-import RoomDirectory from '../src/classes/RoomDirectory';
-import { sampleBookingData, sampleCustomerData, sampleRoomData } from '../src/data/sample-data';
+import { sampleBookingData, sampleRoomData } from '../src/data/sample-data';
 const expect = chai.expect;
 
 
 describe('Booking', () => {
   sampleBookingData
   sampleRoomData
-  let booking1
-  //let roomDirectory, room1, room2, room3, room4
+  let booking1, booking2, room4, room2
 
   beforeEach(() => {
     booking1 = new Booking(sampleBookingData[0], sampleRoomData)
-    // room1 = new Room(sampleRoomData[0])
-    // room2 = new Room(sampleRoomData[1])
-    // room3 = new Room(sampleRoomData[2])
-    // room4 = new Room(sampleRoomData[3])
+    booking2 = new Booking(sampleBookingData[3], sampleRoomData)
+    room2 = new Room(sampleRoomData[1])
+    room4 = new Room(sampleRoomData[3])
     sampleBookingData
     sampleRoomData
   })
@@ -26,37 +23,56 @@ describe('Booking', () => {
   })
   it('should hold the booking id', () => {
     expect(booking1.id).to.equal('5fwrgu4i7k55hl6x8')
+    expect(booking2.id).to.equal('5fwrgu4i7k55hl74l')
   })
   it('should hold the user id', () => {
     expect(booking1.userID).to.equal(1)
+    expect(booking2.userID).to.equal(2)
   })
   it('should hold the date', () => {
     expect(booking1.date).to.equal("2023/01/11")
+    expect(booking2.date).to.equal("2022/01/22")
   })
   it('should hold the reformatted date', () => {
     expect(booking1.reformattedDate).to.equal(1673420400000)
+    expect(booking2.reformattedDate).to.equal(1642834800000)
+  })
+  it('should hold the booking status', () => {
+    expect(booking1.bookingStatus).to.equal("Upcoming Booking")
+    expect(booking2.bookingStatus).to.equal("Past Booking")
   })
   it('should hold the room number', () => {
     expect(booking1.roomNumber).to.equal(20)
+    expect(booking2.roomNumber).to.equal(6)
   })
   it('should hold the room type', () => {
-    //booking1.findRoomInfo(sampleRoomData)
     expect(booking1.roomType).to.equal("residential suite")
+    expect(booking2.roomType).to.equal("junior suite")
   })
   it('should hold the bidet status', () => {
-    //booking1.findRoomInfo(sampleRoomData)
     expect(booking1.bidet).to.equal(false)
+    expect(booking2.bidet).to.equal(true)
+  })
+  it('should hold the bidet message', () => {
+    expect(booking1.bidetMessage).to.equal("bidet not included")
+    expect(booking2.bidetMessage).to.equal("bidet included")
   })
   it('should hold the bed size', () => {
-    //booking1.findRoomInfo(sampleRoomData)
     expect(booking1.bedSize).to.equal("queen")
+    expect(booking2.bedSize).to.equal("queen")
   })
   it('should hold the number of beds', () => {
-   // booking1.findRoomInfo(sampleRoomData)
     expect(booking1.numBeds).to.equal(1)
+    expect(booking2.numBeds).to.equal(1)
   })
   it('should hold the cost per night', () => {
-    //booking1.findRoomInfo(sampleRoomData)
     expect(booking1.costPerNight).to.equal(343.95)
+    expect(booking2.costPerNight).to.equal(397.02)
+  })
+  it('should find the room booked', () => {
+    const roomTest1 = booking1.findRoom(sampleRoomData)
+    expect(roomTest1).to.deep.equal(room4)
+    const roomTest2 = booking2.findRoom(sampleRoomData)
+    expect(roomTest2).to.deep.equal(room2)
   })
 })
