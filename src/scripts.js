4221ae4dec72d379fce071c7e60a75e5a74e4bc0
Eleanor Ruth <ellie.ruth@me.com>
2022-11-10 15:58:20 -0700

    Add fetch data fucntion

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,3 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
@@ -9,15 +7,38 @@ import './css/styles.css';
 import './images/turing-logo.png'
 import './images/overlook-background-image.png'
 import Customer from './classes/Customer';
-import { displayWelcomeMessage } from './domUpdates';
+import { displayBookedRoomsList, displayWelcomeMessage } from './domUpdates';
+import { getData } from './apiCalls';
 
+const customersURL = 'http://localhost:3001/api/v1/customers'
+const roomURL = 'http://localhost:3001/api/v1/rooms'
+const bookingURL = 'http://localhost:3001/api/v1/bookings'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+let apiCustomers
+let apiRooms
+let apiBookings 
+let randomCustomer
+let customer
+
+window.addEventListener('load', fetchData([customersURL, roomURL, bookingURL]))
+
+function fetchData(urls) {
+  Promise.all([getData(urls[0]), getData(urls[1]), getData(urls[2])])
+    .then(data => {
+      apiCustomers = data[0].customers
+      apiRooms = data[1].rooms
+      apiBookings = data[2].bookings
+      randomizeUser(apiCustomers, apiBookings, apiRooms)
+      displayBookedRoomsList(customer)
+    })
+}
 
 //Other Functions
-function randomizeUser(data) {
-  randomUser = data[Math.floor(Math.random() * data.length)]
-  customer = new Customer(randomUser)
+function randomizeUser(customerData, bookingData, roomData) {
+  randomCustomer = customerData[Math.floor(Math.random() * customerData.length)]
+  customer = new Customer(randomCustomer, bookingData, roomData)
   displayWelcomeMessage(customer.name)
-  return user
+  return customer
 }
+
+export {customer}
