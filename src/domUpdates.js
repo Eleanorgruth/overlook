f9cdb8fb7abddf2b96ec6b1b91bc3143a22cca7c
Eleanor Ruth <ellie.ruth@me.com>
2022-11-10 15:57:41 -0700

    Refactor my booking dashboard

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -2,8 +2,9 @@ const myBookingView = document.querySelector('#myBookings')
 const bookARoomView = document.querySelector('#bookARoom')
 const bookingForm = document.querySelector('#bookingForm')
 const bookingOptions = document.querySelector('#bookingInfo')
-const myBookings = document.querySelector('#bookings')
 const welcomeMessage = document.querySelector('#welcomeMessage')
+const bookedRoomsList = document.querySelector('#bookedRoomsList')
+const totalSpent = document.querySelector('#totalSpent')
 
 bookARoomView.addEventListener('click', displayBookingOptions)
 myBookingView.addEventListener('click', displayMyBookings)
@@ -11,7 +12,7 @@ myBookingView.addEventListener('click', displayMyBookings)
 //DOM Updates
 function displayBookingOptions() {
   show([bookingForm, bookingOptions])
-  hide([myBookings])
+  hide([bookedRoomsList])
   selected(bookARoomView)
   unselected(myBookingView)
 }
@@ -20,29 +21,46 @@ function displayMyBookings() {
   selected(myBookingView)
   unselected(bookARoomView)
   hide([bookingForm, bookingOptions])
-  show([myBookings])
+  show([bookedRoomsList])
 }
 
 function displayWelcomeMessage(customerName) {
   welcomeMessage.innerText = `Welcome ${customerName}`
 }
+
+function displayBookedRoomsList(customer) {
+  show([bookedRoomsList])
+  totalSpent.innerText = `Total Spent: $${customer.totalCost.toFixed(2)}`
+  bookedRoomsList.innerHTML = ''
+  customer.bookings.forEach((booking)=> {
+    bookedRoomsList.innerHTML += `
+    <section class="bookings" id="bookings">
+     <p>${booking.bookingStatus}: Room Number: ${booking.roomNumber}</p>
+     <p>Room Info: ${booking.numBeds} ${booking.bedSize}
+      bed(s), ${booking.bidetMessage}</p>
+     <p>Date: ${booking.date}</p>
+     <p>Cost Per Night: $${booking.costPerNight}</p>
+    </section>
+    `
+  })
+}
 //DOM HELPER FUNCTIONS
 function hide(elementList) {
   elementList.forEach((currentElement) => {
-      currentElement.classList.add('hidden')
+    currentElement.classList.add('hidden')
   })
 }
 
 function show(elementList) {
   elementList.forEach((currentElement) => {
-      currentElement.classList.remove('hidden')
+    currentElement.classList.remove('hidden')
   })
 }
 function selected(currentElement) {
-      currentElement.classList.add('selected')
+  currentElement.classList.add('selected')
 }
 function unselected(currentElement) {
-   currentElement.classList.remove('selected')
+  currentElement.classList.remove('selected')
 }
 
-export { displayWelcomeMessage }
+export { displayWelcomeMessage, displayBookedRoomsList }
