1e8b068354c25af6cd6c1989a33e8f0b96aead26
Eleanor Ruth <ellie.ruth@me.com>
2022-11-09 17:58:07 -0700

    Create Room class

--- src/classes/Room.js
+++ src/classes/Room.js
@@ -1,18 +1,20 @@
 class Room {
   constructor(roomInfo) {
-    this.roomNumber = roomInfo.roomNumber
+    this.roomNumber = roomInfo.number
     this.roomType = roomInfo.roomType
     this.bidetStatus = roomInfo.bidet
     this.bedSize = roomInfo.bedSize
     this.numBeds = roomInfo.numBeds
     this.costPerNight = roomInfo.costPerNight
     // this.allBookingsInfo = allBookingsInfo
-    this.bookingsInfo = []
+    // this.bookingsInfo = []
   }
-  getBookingInfo() {
-    const currentRoomBookings = this.allBookingsInfo.filter((booking)=>{
-      return booking.roomNumber === this.roomNumber
-    })
-    this.bookingsInfo = currentRoomBookings
-  }
+  // getBookingInfo() {
+  //   const currentRoomBookings = this.allBookingsInfo.filter((booking)=>{
+  //     return booking.roomNumber === this.roomNumber
+  //   })
+  //   this.bookingsInfo = currentRoomBookings
+  // }
 }
+
+export default Room
