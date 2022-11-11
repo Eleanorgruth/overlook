7d6d6d173e3900a52514e4b9abf1fac74beb2454
Eleanor Ruth <ellie.ruth@me.com>
2022-11-11 13:47:47 -0700

    Write filter function

--- src/classes/RoomDirectory.js
+++ src/classes/RoomDirectory.js
@@ -1,14 +1,31 @@
-import Room from "./Room"
-
 class RoomDirectory {
-  constructor (allRoomData) {
-    this.rooms = this.createRooms(allRoomData)
+  constructor(allRoomData) {
+    this.rooms = allRoomData
+    this.filteredRooms = []
   }
- createRooms(allRoomData) {
-  return allRoomData.map((roomInfo) => {
-    return new Room(roomInfo)
-  })
- }
+  findAvalibleRooms(date, roomType, bookingData) {
+    const filteredRooms = this.rooms.reduce((avalibleRooms, room) => {
+     const roomNumbersBooked = bookingData.reduce((roomNumbersBooked, booking)=> {
+      const date2 = new Date(date)  
+      const date1 = new Date(booking.date)
+      if (date1.getTime() === date2.getTime()) {
+       roomNumbersBooked.push(booking.roomNumber)
+      }
+       return roomNumbersBooked
+      }, [])
+      if (!roomNumbersBooked.includes(room.number)) {
+         avalibleRooms.push(room)
+      }
+      return avalibleRooms
+    }, []).filter((room)=>{
+      if (roomType === room.roomType) {
+         return room
+      } else if (roomType === "all" || roomType === "Choose Type...") {
+        return room
+      }
+    })
+    this.filteredRooms = filteredRooms
+  }
 }
 
 export default RoomDirectory
