ff8405c727be6ab4aa858c6233d9c3146f51b5a0
Eleanor Ruth <ellie.ruth@me.com>
2022-11-11 19:33:31 -0700

    Post calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,6 @@
+import Customer from "./classes/Customer"
+import { bookingURL, displayBookedRoomsList, customer, apiRooms, getCustomerData } from "./scripts"
+
 const getData = (url) => {
   return fetch(url)
     .then(response => {
@@ -11,4 +14,28 @@ const getData = (url) => {
     })
 }
 
-export { getData }
+const postBooking = (postData) => {
+  return fetch(bookingURL, {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: { 'Content-Type': 'application/json' }
+  })
+      .then(response => {
+          if (!response.ok) {
+              throw new Error(`Sorry, something went wrong. ${response.status}: ${response.statusText}`)
+          }
+          return response.json()
+      })
+      .then(test => getData(bookingURL))
+      .then(data => {
+        console.log("DATA", data)
+        const test1 = getCustomerData(customer)
+       const newCustomer = new Customer(test1, data.bookings, apiRooms)
+       displayBookedRoomsList(newCustomer)
+      })
+      .catch(err => {
+          console.log('Fetch Error: ', err)
+      })
+}
+
+export { getData, postBooking }
