b9a1f0a4a5ece4906e559c6c7af2636d37e45499
Eleanor Ruth <ellie.ruth@me.com>
2022-11-14 13:28:54 -0700

    Add customer without bookings to test sad paths

--- test/Customer-test.js
+++ test/Customer-test.js
@@ -9,12 +9,21 @@ import {
 } from '../src/data/sample-data'
 
 describe('Customer', () => {
-  let customer1, customer2, booking1, booking2, booking3, booking4, booking5
+  let customer1,
+    customer2,
+    customer5,
+    booking1,
+    booking2,
+    booking3,
+    booking4,
+    booking5
 
   beforeEach(() => {
     customer1 = new Customer(sampleCustomerData[0], 
       sampleBookingData, sampleRoomData)
     customer2 = new Customer(sampleCustomerData[1], 
+      sampleBookingData, sampleRoomData)
+    customer5 = new Customer(sampleCustomerData[4], 
       sampleBookingData, sampleRoomData)
     booking1 = new Booking(sampleBookingData[0], sampleRoomData)
     booking2 = new Booking(sampleBookingData[1], sampleRoomData)
@@ -28,17 +37,25 @@ describe('Customer', () => {
   it('should hold a customer id', () => {
     expect(customer1.id).to.equal(1)
     expect(customer2.id).to.equal(2)
+    expect(customer5.id).to.equal(5)
   })
   it('should hold a customer name', () => {
     expect(customer1.name).to.equal('Leatha Ullrich')
     expect(customer2.name).to.equal('Rocio Schuster')
+    expect(customer5.name).to.equal('Rhiannon Little')
   })
-  it('should hold a list of all the customers bookings in chronological order', () => {
+  it('should list all the customers bookings in chronological order', () => {
     expect(customer1.bookings).to.deep.equal([booking1, booking3, booking2])
-    expect(customer2.bookings).to.deep.equal([booking5, booking4])
+    expect(customer2.bookings).to.deep.equal([booking4, booking5])
+  })
+  it('should list an empty array if customer has no bookings', () => {
+    expect(customer5.bookings).to.deep.equal([])
   })
   it('should be able to calculate the cost of all customer bookings', () => {
     expect(customer1.totalCost).to.equal(1170.41)
     expect(customer2.totalCost).to.equal(794.04)
+  })
+  it('should calculate cost to 0 if customer has no bookings', () => {
+    expect(customer5.totalCost).to.equal(0)
   })
 })
