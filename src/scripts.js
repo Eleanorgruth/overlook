8dd4696c0fd375f1584dcbc1bd0747ab37e9cc29
Eleanor Ruth <ellie.ruth@me.com>
2022-11-13 21:21:12 -0700

    Add sign-out feature

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,17 @@
 import './css/styles.css'
 import './images/overlook-background-image.png'
 import Customer from './classes/Customer'
-import { displayBookedRoomsList, setMinimumDate, bookingOptions, dateSelection, updateBookedRoomsList, bookingConfirmation, displayMyBookings, giveUserError } from './domUpdates'
+import {
+  displayBookedRoomsList,
+  setMinimumDate,
+  bookingOptions,
+  dateSelection,
+  updateBookedRoomsList,
+  bookingConfirmation,
+  displayMyBookings,
+  giveUserError,
+  show
+} from './domUpdates'
 import { getData } from './apiCalls'
 import RoomDirectory from './classes/RoomDirectory'
 
@@ -12,7 +22,6 @@ const bookingURL = 'http://localhost:3001/api/v1/bookings'
 let apiCustomers
 let apiRooms
 let apiBookings 
-let randomCustomer
 let customer
 let roomDirectory
 
@@ -21,22 +30,26 @@ const usernameInput = document.querySelector('#username')
 const passwordInput = document.querySelector('#password')
 
 signInButton.addEventListener('click', checkSignInInfo)
+bookingOptions.addEventListener('click', bookRoom)
+
+passwordInput.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    checkSignInInfo()
+  }
+})
 
 function checkSignInInfo() {
   let username = usernameInput.value.slice(0, 8) 
   let usernameID = Number(usernameInput.value.slice(8))
-  console.log("USERNAME", username)
-  console.log("USERNAMEID", usernameID)
-  if (username ==="customer" && usernameID > 0 && usernameID <= 50 && passwordInput.value === "overlook2021") {
+  if (username === "customer" && usernameID > 0 && usernameID <= 50
+   && passwordInput.value === "overlook2021") {
     let customerURL = customersURL + usernameID
     fetchData([customerURL, roomURL, bookingURL])
-    console.log("HELP")
   } else {
-    //display error "invalid password or userName, try again"
+    const signInErorr = document.querySelector('#signInErorr')
+    show([signInErorr])
   }
 }
-//window.addEventListener('load', fetchData([customersURL, roomURL, bookingURL]))
-bookingOptions.addEventListener('click', bookRoom)
 
 function fetchData(urls) {
   Promise.all([getData(urls[0]), getData(urls[1]), getData(urls[2])])
@@ -52,39 +65,33 @@ function fetchData(urls) {
     })
 }
 
-//Other Functions
-// function randomizeUser(customerData, bookingData, roomData) {
-//   randomCustomer = customerData[Math.floor(Math.random() * customerData.length)]
-//   customer = new Customer(randomCustomer, bookingData, roomData)
-//   displayWelcomeMessage(customer.name)
-//   return customer
-// }
-
 function bookRoom(event) {
   if (event.target.classList[0] === 'book-room-btn') {
-    const postData = {userID: customer.id, date: getDateForPost(dateSelection.value), roomNumber: Number(event.target.id)}
-    console.log(postData)
+    const postData = {
+      userID: customer.id,
+      date: getDateForPost(dateSelection.value),
+      roomNumber: Number(event.target.id)
+    }
     return fetch(bookingURL, {
       method: 'POST',
       body: JSON.stringify(postData),
       headers: { 'Content-Type': 'application/json' }
-  })
+    })
       .then(response => {
-          if (!response.ok) {
-            throw new Error(`Sorry, something went wrong. ${response.status}: ${response.statusText}`)
-          }
-          return response.json()
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}: ${response.statusText}`)
+        }
+        return response.json()
       })
-      .then(test => getData(bookingURL))
+      .then(() => getData(bookingURL))
       .then(data => {
-        console.log("DATA", data.bookings)
-       customer = new Customer(apiCustomers, data.bookings, apiRooms)
-       updateBookedRoomsList(customer)
-       roomDirectory = new RoomDirectory(apiRooms, data.bookings)
-       bookingConfirmation()
-       setTimeout(()=>{
-        displayMyBookings()
-       }, 3000)
+        customer = new Customer(apiCustomers, data.bookings, apiRooms)
+        updateBookedRoomsList(customer)
+        roomDirectory = new RoomDirectory(apiRooms, data.bookings)
+        bookingConfirmation()
+        setTimeout(()=>{
+          displayMyBookings()
+        }, 3000)
       })
       .catch(err => {
         giveUserError()
@@ -93,12 +100,16 @@ function bookRoom(event) {
   }
 }
 
-// function getCustomerData(customer) {
-//   return apiCustomers.find((currentCustomer)=> {
-//     return currentCustomer.id === customer.id
-//   })
-// }
 function getDateForPost(date) {
- return date.split('-').join('/')
+  return date.split('-').join('/')
 }
-export {customer, roomDirectory, apiBookings, bookingURL, displayBookedRoomsList, apiRooms, getDateForPost }
+
+export {
+  customer,
+  roomDirectory,
+  apiBookings,
+  bookingURL,
+  displayBookedRoomsList,
+  apiRooms,
+  getDateForPost
+}
